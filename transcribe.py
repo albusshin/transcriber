44a0b4f83005281bdb2e34c65fccc7af8f29d05c
Tian Xin <tian.xin@fb.com>
2023-06-17 22:26:48 -0700

    transcribe: parameterized cli arguments, added time outputs, use temp dir

--- transcribe.py
+++ transcribe.py
@@ -3,18 +3,24 @@
 import argparse
 import re
 import json
-from pydub import AudioSegment
-from pyannote.audio import Pipeline
-import whisper
-import torch
 import os
+from datetime import timedelta
+import tempfile
+from timeit import default_timer as timer
 from datetime import timedelta
 
 
-def perform(input_wav_file: str):
+def millisec(time_str):
+    spl = time_str.split(":")
+    return (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2])) * 1000)
+
+
+def perform(input_wav_file: str, hugging_face_access_token: str, output_txt_dir: str):
     print("Prepping file:", input_wav_file)
 
     spacermilli = 2000
+    from pydub import AudioSegment
+
     spacer = AudioSegment.silent(duration=spacermilli)
 
     audio = AudioSegment.from_wav(input_wav_file)
@@ -24,25 +30,25 @@ def perform(input_wav_file: str):
 
     audio.export(prepped_input_wav_file, format="wav")
 
-    print("File Prepped:", prepped_input_wav_file)
+    print("File Prepped:", os.path.join(os.getcwd(), prepped_input_wav_file))
 
-    hugging_face_access_token = "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"
+    print("Loading Pipeline module")
+    timer_beginning = timer()
+    from pyannote.audio import Pipeline
 
+    print("Pipeline module loaded in", timedelta(seconds=timer() - timer_beginning))
+
+    print("Loading pretrained pipeline")
+    timer_beginning = timer()
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization", use_auth_token=hugging_face_access_token
     )
+    print("Pretrained pipeline loaded in", timedelta(seconds=timer() - timer_beginning))
 
-    dz_pipeline_file = {"uri": "blabla", "audio": prepped_input_wav_file}
-    dz = pipeline(dz_pipeline_file)
-
-    print(*list(dz.itertracks(yield_label=True))[:10], sep="\n")
-
-    def millisec(timeStr):
-        spl = timeStr.split(":")
-        s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2])) * 1000)
-        return s
-
-    dzs = str(dz).splitlines()
+    print("Diarizing...")
+    timer_beginning = timer()
+    dzs = str(pipeline({"uri": "blabla", "audio": prepped_input_wav_file})).splitlines()
+    print("Diarized in", timedelta(seconds=timer() - timer_beginning))
 
     groups = []
     g = []
@@ -64,8 +70,9 @@ def perform(input_wav_file: str):
             lastend = end
     if g:
         groups.append(g)
-    print(*groups, sep="\n")
 
+    print("Exporting mini audio files...")
+    timer_beginning = timer()
     audio = AudioSegment.from_wav(prepped_input_wav_file)
     gidx = -1
     for g in groups:
@@ -75,18 +82,39 @@ def perform(input_wav_file: str):
         end = millisec(end)  # - spacermilli
         gidx += 1
         audio[start:end].export(str(gidx) + ".wav", format="wav")
-        print(f"group {gidx}: {start}--{end}")
+
+    del dzs, audio, pipeline
+    print("Mini audio files exported in ", timedelta(seconds=timer() - timer_beginning))
+
+    print("Starting transcribing using Whisper.")
+    import whisper
+    import torch
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Loading model...")
+    timer_beginning = timer()
     model = whisper.load_model("large-v2", device=device)
+    print("Model loaded in", timedelta(seconds=timer() - timer_beginning))
 
-    for i in range(len(groups)):
+    len_groups = len(groups)
+    print(f"Transcribing {len_groups} mini audio files...")
+    timer_beginning = timer()
+    for i in range(len_groups):
         audiof = str(i) + ".wav"
+        timer_beginning_i = timer()
+        print(f"Transcribing audio file\t {i} of {len_groups}...")
         result = model.transcribe(
             audio=audiof, language="en", word_timestamps=True
         )  # , initial_prompt=result.get('text', ""))
+
+        print(
+            f"Audio file \t{i} transcribed. Time:",
+            timedelta(seconds=timer() - timer_beginning_i),
+        )
         with open(str(i) + ".json", "w") as outfile:
             json.dump(result, outfile, indent=4)
+
+    print("Total transcription time:", timedelta(seconds=timer() - timer_beginning))
 
     def timeStr(t):
         return "{0:02d}:{1:02d}:{2:06.2f}".format(
@@ -124,11 +152,19 @@ def perform(input_wav_file: str):
                         continue
                     start = (shift + w["start"] * 1000.0) / 1000.0
 
-    with open(f"capspeaker.txt", "w", encoding="utf-8") as file:
+    output_file_path = os.path.join(
+        os.path.expanduser(output_txt_dir),
+        f"{os.path.basename(input_wav_file)}.transcribed.txt",
+    )
+
+    output_directory_path = os.path.dirname(output_file_path)
+    if not os.path.exists(output_directory_path):
+        os.makedirs(output_directory_path)
+
+    with open(output_file_path, "w", encoding="utf-8") as file:
         s = "".join(txt)
         file.write(s)
-        print("captions saved to capspeaker.txt:")
-        print(s + "\n")
+        print(f"Captions saved to {output_file_path}")
 
 
 def parse_arguments():
@@ -136,10 +172,27 @@ def parse_arguments():
     parser.add_argument(
         "--input-wav-file", required=True, help="Path to the input WAV file"
     )
+    parser.add_argument(
+        "--hugging-face-access-token",
+        required=True,
+        help="Access token from https://hf.co",
+    )
+    parser.add_argument(
+        "--output-txt-dir",
+        required=True,
+        help="Directory to output the transcribed text file",
+    )
+
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_arguments()
     print("Input file: " + args.input_wav_file)
-    perform(args.input_wav_file)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        print(f"Changing into directory {temp_dir}")
+        os.chdir(temp_dir)
+        perform(
+            args.input_wav_file, args.hugging_face_access_token, args.output_txt_dir
+        )
